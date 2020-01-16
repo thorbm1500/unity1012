905ffa3cd4d5afbd230696870f2375e4c617734b
thorbm1500 <synxi@outlook.dk>
2020-01-16 13:40:51 +0100

    Script Test

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+// Ja taaaaaaak 
     }
 
     // Update is called once per frame
